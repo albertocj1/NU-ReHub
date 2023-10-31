ada226fa325b9166b4444d81248e7c02b41c0282
albertocj1 <albertochristianjoshua@gmail.com>
2023-10-31 23:34:32 +0800

    Update LogIn.java

---
+++ DSA Project/src/LogIn.java
@@ -0,0 +1,36 @@
+public class LogIn {
+    class User {
+        String username;
+        String password;
+        User next;
+    
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+            this.next = null;
+        }
+    }
+
+    class UserLinkedList {
+        User head;
+
+        public void addUser(String username, String password) {
+            User newUser = new User(username, password);
+            newUser.next = head;
+            head = newUser;
+        }
+
+        public boolean validateUser(String username, String password) {
+            User current = head;
+            while (current != null) {
+                if (current.username.equals(username) && current.password.equals(password)){
+                    return true; // if user exists
+                }
+                current = current.next;
+            }
+            return false; // if user does not exist.
+        }
+
+    }
+    
+}
