e132defe24530b3563ea168acbf9a9f0941a7772
rajkyutiebaby <rodneyestrada2425@gmail.com>
2023-10-31 16:53:28 +0800

    warmup

--- App.java
+++ App.java
@@ -1,6 +1,8 @@
-class Simple{  
+class App{  
     public static void main(String args[]){  
         System.out.println("Hello Java");
         System.out.println("ampoge q");
-    }  
+        System.out.println("asd");
+        
+    } 
 }  
