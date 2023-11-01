cee8059f92aafd915c2737b7d6dbefdf54217be3
albertocj1 <albertochristianjoshua@gmail.com>
2023-11-01 22:12:41 +0800

    LogIn

--- DSA Project/src/LogIn.java
+++ DSA Project/src/LogIn.java
@@ -1,55 +1,10 @@
-import java.util.Scanner;
-
 public class LogIn {
-    class User {
-        String username;
-        String password;
-        User next;
-    
-        public User(String username, String password) {
-            this.username = username;
-            this.password = password;
-            this.next = null;
-        }
+
+    public void registerUser(String string, String string2) {
     }
 
-    class UserLinkedList {
-        User head;
-
-        public void addUser(String username, String password) {
-            User newUser = new User(username, password);
-            newUser.next = head;
-            head = newUser;
-        }
-
-        public boolean validateUser(String username, String password) {
-            User current = head;
-            while (current != null) {
-                if (current.username.equals(username) && current.password.equals(password)){
-                    return true; // if user exists
-                }
-                current = current.next;
-            }
-            return false; // if user does not exist.
-        }
-
-        public void registerUser(Scanner scanner) {
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
-        
-        // Check if username is already taken
-        User current = head;
-             while (current != null) {
-                if (current.username.equals(username)) {
-                    System.out.println("Username already exists. Please choose a different username.");
-                        return;
-                }
-
-                 current = current.next;
-             }
-    
-        }
-
+    public boolean validateUser(String username, String password) {
+        return false;
     }
     
 }
---
+++ DSA Project/src/Register.java
@@ -0,0 +1,58 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Register {
+    public HashMap <String, String> users;
+
+    public Register(){
+        users = new HashMap<>();
+    }
+
+    public void registerUser(Scanner scanner) {
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+    
+        if (!users.containsKey(username)) {
+            System.out.print("Enter password: ");
+            String password = scanner.nextLine();
+    
+            users.put(username, password);
+            System.out.println("User registered successfully!");
+        } else {
+            System.out.println("Username already exists. Please choose another username.");
+        }
+    }
+
+    public boolean validateUser(String username, String password) {
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            return true; 
+        }
+        return false; 
+    }
+    public static void main(String[] args) {
+        Register registrationSystem = new Register();
+        Scanner scanner = new Scanner(System.in);
+
+        // Register new users
+        registrationSystem.registerUser(scanner);
+
+        boolean loggedIn = false;
+
+
+    while(!loggedIn){
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        if (registrationSystem.validateUser(username, password)) {
+            System.out.println("Login successful!");
+            loggedIn = true;
+        } else {
+            System.out.println("Invalid credentials!");
+
+        }
+    }
+
+        scanner.close(); // Don't forget to close the scanner
+    }
+}
