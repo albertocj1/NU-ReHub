4b24f7b6eef5f8644e377cff7e0eb7ee1e678f4a
rajkyutiebaby <rodneyestrada2425@gmail.com>
2023-11-01 22:17:16 +0800

    goodshits
    kulang pa doctype

--- DSA Project/src/DataSample.java
+++ DSA Project/src/DataSample.java
@@ -3,51 +3,163 @@ public class DataSample {
     public Object[][] dataSample = new Object[][] {
         {
             "researchTitle", "A Comparative Study of Machine Learning Algorithms for Predictive Analytics in E-Commerce",
-            "researchAuthor", "John Smith",
+            "researchAuthor", "Maria Santos",
             "yearPublished", "2020",
             "researchDOI", "10.5678/abcd1234",
             "researchCourse", "BSCS-ML",
-            "researchGenres", new String[]{"Predictive Analytics", "E-Commerce", "Machine Learning"}
+            "researchGenres", new String[]{"Machine Learning"}
         },
         {
             "researchTitle", "Scalability and Performance Optimization in Modern Web Applications",
-            "researchAuthor", "Michael Anderson",
+            "researchAuthor", "Juan Cruz",
             "yearPublished", "2018",
             "researchDOI", "10.5678/efgh5678",
-            "researchCourse", "Web Development",
-            "researchGenres", new String[]{"Scalability", "Web Applications", "Performance Optimization"}
+            "researchCourse", "BSIT-MWA",
+            "researchGenres", new String[]{"Web Development"}
         },
         {
             "researchTitle", "Mitigating Cybersecurity Threats in Internet of Things (IoT) Devices",
-            "researchAuthor", "Sarah Martinez",
+            "researchAuthor", "Luz Reyes",
             "yearPublished", "2017",
             "researchDOI", "10.5678/ijkl9012",
-            "researchCourse", "Cybersecurity",
-            "researchGenres", new String[]{"IoT Security", "Cyber Threats", "Network Security"}
+            "researchCourse", "BSCS-DF",
+            "researchGenres", new String[]{"Cybersecurity"}
         },
         {
             "researchTitle", "Advancements in Natural Language Processing using Deep Learning Techniques",
-            "researchAuthor", "David Kim",
+            "researchAuthor", "Antonio Garcia",
             "yearPublished", "2019",
             "researchDOI", "10.5678/mnop3456",
-            "researchCourse", "Machine Learning",
-            "researchGenres", new String[]{"Natural Language Processing", "Deep Learning", "Text Mining"}
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Machine Learning"}
         },
         {
             "researchTitle", "Exploring Generative Adversarial Networks (GANs) for Image Synthesis",
-            "researchAuthor", "Jessica Lee",
+            "researchAuthor", "Rosario Villanuava",
             "yearPublished", "2016",
             "researchDOI", "10.5678/qrst5678",
-            "researchCourse", "Deep Learning",
-            "researchGenres", new String[]{"Generative Adversarial Networks", "Image Synthesis", "Computer Vision"}
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Machine Learning"}
         },
         {
             "researchTitle", "Ethical Considerations in AI: Ensuring Fairness and Accountability in Autonomous Systems",
-            "researchAuthor", "Daniel Thompson",
+            "researchAuthor", "Eduardo Ramos",
             "yearPublished", "2021",
             "researchDOI", "10.5678/uvwx1234",
-            "researchCourse", "Artificial Intelligence",
-            "researchGenres", new String[]{"Ethical AI", "Autonomous Systems", "Fairness in AI"}
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Machine Learning"}
+        },
+        {
+            "researchTitle", "Enhancing Mobile App Security with Machine Learning",
+            "researchAuthor", "Consuelo Hernandez",
+            "yearPublished", "2022",
+            "researchDOI", "10.5678/ab123cd",
+            "researchCourse", "BSIT-MWA",
+            "researchGenres", new String[]{"Cybersecurity"}
+        },
+        {
+            "researchTitle", "Predictive Analytics in Game Development",
+            "researchAuthor", "Alejandro Cruzado",
+            "yearPublished", "2019",
+            "researchDOI", "10.5678/ef123gh",
+            "researchCourse", "BSIT-MAA",
+            "researchGenres", new String[]{"Game Development", "Machine Learning"}
+        },
+        {
+            "researchTitle", "Digital Forensics and Cyber Threat Detection",
+            "researchAuthor", "Carmen Torres",
+            "yearPublished", "2018",
+            "researchDOI", "10.5678/ij123kl",
+            "researchCourse", "BSCS-DF",
+            "researchGenres", new String[]{"Cybersecurity"}
+        },
+        {
+            "researchTitle", "Natural Language Processing for Sentiment Analysis",
+            "researchAuthor", "Rodrigo Fernandez",
+            "yearPublished", "2020",
+            "researchDOI", "10.5678/mn123op",
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Machine Learning"}
+        },
+        {
+            "researchTitle", "Machine Learning in Healthcare Data Analysis",
+            "researchAuthor", "Amy Rodriguez",
+            "yearPublished", "2021",
+            "researchDOI", "10.5678/qr123st",
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Data Science", "Machine Learning"}
+        },
+        {
+            "researchTitle", "Visual Effects and Animation Techniques in Game Development",
+            "researchAuthor", "Jose Mendoza",
+            "yearPublished", "2017",
+            "researchDOI", "10.5678/uv123wx",
+            "researchCourse", "BSIT-MAA",
+            "researchGenres", new String[]{"Game Development"}
+        },
+        {
+            "researchTitle", "Securing IoT Devices in Data Forensics",
+            "researchAuthor", "Aurora Dela Cruz",
+            "yearPublished", "2018",
+            "researchDOI", "10.5678/yz123ab",
+            "researchCourse", "BSCS-DF",
+            "researchGenres", new String[]{"Cybersecurity"}
+        },
+        {
+            "researchTitle", "Machine Learning for Financial Data Analysis",
+            "researchAuthor", "Manuel Gonzales",
+            "yearPublished", "2019",
+            "researchDOI", "10.5678/bc234de",
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Data Science", "Machine Learning"}
+        },
+        {
+            "researchTitle", "Responsive Web Design and Mobile App Development",
+            "researchAuthor", "Luningning Abella",
+            "yearPublished", "2022",
+            "researchDOI", "10.5678/ef234gh",
+            "researchCourse", "BSIT-MWA",
+            "researchGenres", new String[]{"Web Development"}
+        },
+        {
+            "researchTitle", "Game Engine Development for Virtual Reality Experiences",
+            "researchAuthor", "Isidro Espinoza",
+            "yearPublished", "2016",
+            "researchDOI", "10.5678/ij234kl",
+            "researchCourse", "BSIT-MAA",
+            "researchGenres", new String[]{"Game Development"}
+        },
+        {
+            "researchTitle", "Data Privacy and Security in Machine Learning",
+            "researchAuthor", "Milagros Ramos",
+            "yearPublished", "2023",
+            "researchDOI", "10.5678/cd345ef",
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Cybersecurity", "Machine Learning"}
+        },
+        {
+            "researchTitle", "Big Data Analysis for E-Commerce Optimization",
+            "researchAuthor", "Guillermo Lim",
+            "yearPublished", "2022",
+            "researchDOI", "10.5678/gh456ij",
+            "researchCourse", "BSCS-ML",
+            "researchGenres", new String[]{"Data Science"}
+        },
+        {
+            "researchTitle", "Mobile App Development for Game-Based Learning",
+            "researchAuthor", "Teresa Sison",
+            "yearPublished", "2018",
+            "researchDOI", "10.5678/kl567mn",
+            "researchCourse", "BSIT-MAA",
+            "researchGenres", new String[]{"Game Development"}
+        },
+        {
+            "researchTitle", "Forensic Analysis of Cyber Attacks in IoT Networks",
+            "researchAuthor", "Reynaldo Caballero",
+            "yearPublished", "2019",
+            "researchDOI", "10.5678/op678qr",
+            "researchCourse", "BSCS-DF",
+            "researchGenres", new String[]{"Cybersecurity"}
         }
     };
 
